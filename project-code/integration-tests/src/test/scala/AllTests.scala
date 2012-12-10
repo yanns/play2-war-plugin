bc2c663c3bcf34a55a9187fe3f5394ecb6e7a718
Damien Lecan <dev@dlecan.com>
2012-12-10 15:13:52 +0100

    Introduce local JBoss container

--- project-code/integration-tests/src/test/scala/AllTests.scala
+++ project-code/integration-tests/src/test/scala/AllTests.scala
@@ -500,5 +500,6 @@ class Tomcat7027Tests extends AbstractTomcat7x {
 @RunWith(classOf[JUnitRunner])
 class JBoss71xTests extends AbstractPlay2WarTests with Servlet30Container {
   override def containerUrl = "http://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.zip"
+  override def containerFileNameInCloudbeesCache = Option("jboss-as-7.1.1.Final.zip")
   override def containerName = "jboss71x"
 }
